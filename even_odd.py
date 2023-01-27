013521dbaf523ec7737420043f4d8c90a5462f11
Haruto Mori <baystarsman@outlook.jp>
2023-01-27 11:34:54 +0100

    add error handling feature

--- even_odd.py
+++ even_odd.py
@@ -1,6 +1,11 @@
-num = int(input("enter the integer number: "))
+while True:
+    try:
+        num = int(input("enter the integer number: "))
+        break     
+    except ValueError as Verr:
+        print("plz enter the integer number: ", Verr)
 
 if num % 2 == 0:
-    print(f"{num} is even")
+        print(f"{num} is even")
 else:
     print(f"{num} is odd")
